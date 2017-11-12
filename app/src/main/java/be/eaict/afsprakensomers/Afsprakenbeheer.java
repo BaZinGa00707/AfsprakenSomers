f8b602a87de0a18ca6d374a9e34894b799d32a0e
Inias Somers <inias--@hotmail.com>
2017-11-12 19:34:54 +0100

    First commit

---
+++ app/src/main/java/be/eaict/afsprakensomers/Afsprakenbeheer.java
@@ -0,0 +1,13 @@
+package be.eaict.afsprakensomers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Afsprakenbeheer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_afsprakenbeheer);
+    }
+}
